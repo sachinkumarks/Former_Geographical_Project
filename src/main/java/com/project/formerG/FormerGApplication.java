88b4196bfc6333b5d758c4b93ef1c1fe93d3a6b7
sachinkumarks <sachinkumar1999.b@gmail.com>
2024-05-05 16:18:55 +0530

    Changed spring app name

--- src/main/java/com/project/formerG/FormerGApplication.java
+++ src/main/java/com/project/formerG/FormerGApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class FormerGApplication implements Runnable{
 	public void run() {
-		System.out.println("FormerG__Application_Started");
+		System.out.println("FormerG___Application_Started");
 	}
 
 	public static void main(String[] args) {
