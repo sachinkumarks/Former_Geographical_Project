45b4b92e11825e3588fbc4fda89b85fefccea841
2239635 <2239635@LTIN418663.cts.com>
2024-05-05 16:04:55 +0530

    Changed spring boot application name

--- src/main/java/com/project/formerG/FormerGApplication.java
+++ src/main/java/com/project/formerG/FormerGApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class FormerGApplication implements Runnable{
 	public void run() {
-		System.out.println("FormerGApplication_Started");
+		System.out.println("FormerG_Application_Started");
 	}
 
 	public static void main(String[] args) {
